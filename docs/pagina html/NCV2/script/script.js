2d1fbe223bd0fd68f1e3952d00aaa395c5f2a1c0
Igor Correa Trifilio Campos <igorcorreatrifilioc@gmail.com>
2023-03-06 21:53:57 -0300

    major changes in calculator

--- docs/pagina html/NCV2/script/script.js
+++ docs/pagina html/NCV2/script/script.js
@@ -4,42 +4,60 @@ let clean = document.querySelector('#cl');
 let dimi = document.querySelector('#tirar');
 let res = document.querySelector('#rr')
 
+
+
 //Eventos
-clean.addEventListener('click', () => {
-    p.innerHTML = ''
-});
+clean.addEventListener('click', gambisSupre);
+document.addEventListener('DOMContentLoaded', gambisSupre);
+document.addEventListener('keypress', inserir);
+document.addEventListener('keydown', nonModifier);
 
-document.addEventListener('keydown',inserir)
-
-dimi.addEventListener('click', () => {
-    let ap = p.innerHTML
-    p.innerText = ap.substring(0, ap.length - 1)
-})
-res.addEventListener('click', resultado)
+dimi.addEventListener('click', deletar);
+res.addEventListener('click', resultado);
 
 //Funções
-function resultado(resu) {
+ function resultado(resu) {
     if (resu = p.innerHTML) {
         return p.innerHTML = eval(resu).toLocaleString('pt-BR', {
             minimumFractionDigits: 0,
             maximumFractionDigits: 2,
         });
-    }
-}
+     };
+};
+function gambisSupre() {
+    p.innerHTML = ''
+};
+function deletar() {
+    let ap = p.innerHTML
+    ap.length = 0;
+    p.innerText = ap.substring(0, ap.length - 1)
+    if (ap.length == 0) {
+        p.innerText = ap;
+    };
+};
+
 function insert(nn) {
-    let num = p.innerHTML
-    p.innerHTML = num + nn  
+    let num =  p.innerHTML
+    p.innerHTML = num + nn
 }
 
 function inserir(event) {
-    let key = event.key; 
+    let key = event.key;
     let num = p.innerHTML;
-    if (key !== 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 0) {
-       p.innerHTML=num
+    if (isNaN(key)) {
+        p.innerHTML = num;
     } else {
-        `${num + key}`
-    } if (key == 'Enter') {
-        alert('oi')
-    }
+        p.innerHTML = num + key;
+    }  
+    if (key == 'Enter') {
+        p.innerText = num;
+        return resultado();
+    };
 }
 
+function nonModifier(event) {
+    let key = event.key;
+    if (key == 'Backspace') {
+        return deletar();
+    };
+};
